e24a59155c62448f397eb4aede343af28fa18609
sergii zhuravskyi <sergi.zhuravski@gmail.com>
2017-12-01 14:01:15 +0000

    workig example

---
+++ Steps/AuthorizationSteps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace DotNetCoreSpecflow.Steps
+{
+    [Binding]
+    public class AuthorizationSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::1231231");
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        {
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::1231231");
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            throw new Exception();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::::::1231231");
+        }
+
+
+    }
+}
